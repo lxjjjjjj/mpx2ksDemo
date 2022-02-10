438fa99e7c5b70775758783439215c08c4025572
sarahliu <sarahliu@didiglobal.com>
2022-02-10 16:13:34 +0800

    修改

---
+++ config/index.js
@@ -0,0 +1,26 @@
+const mpxLoaderConf = require('./mpxLoader.conf')
+const mpxPluginConf = require('./mpxPlugin.conf')
+const userConf = require('./user.conf')
+
+const supportedModes = ['wx', 'ali', 'swan', 'qq', 'tt', 'qa', 'jd', 'dd','ks']
+
+if (userConf.transWeb) {
+  supportedModes.push('web')
+}
+
+if (userConf.transHummer) {
+  supportedModes.push('tenon')
+}
+
+const options = {
+  userConf,
+  mpxLoaderConf,
+  mpxPluginConf,
+  supportedModes
+}
+
+if (userConf.needDll) {
+  options.dllConf = require('./dll.conf')
+}
+
+module.exports = options
